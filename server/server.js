687cefcb377412504dbfaa1210e743e9816bda30
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-06-11 18:14:43 +0200

    server: user auth

--- server/server.js
+++ server/server.js
@@ -1,10 +1,71 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const { check, validationResult, body, oneOf } = require("express-validator"); // validation middleware
+
+const surveyDao = require("./survey-dao");
+const userDao = require("./user-dao");
+
+/* PASSPORT set up */
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    userDao.getUser(username, password).then((user) => {
+      if (!user)
+        return done(null, false, {
+          message: "Incorrect username and/or password.",
+        });
+
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  userDao
+    .getUserById(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+});
 
 // init express
 const app = new express();
 const port = 3001;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+// session middleware
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+
+  return res.status(401).json({ error: "not authenticated" });
+};
+
+app.use(
+  session({
+    secret: "secret sentence not to be shared",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*** APIs ***/
 
 // activate the server
 app.listen(port, () => {
---
+++ server/user-dao.js
@@ -0,0 +1,47 @@
+"use strict";
+/* Data Access Object (DAO) module for accessing users */
+
+const db = require("./db");
+const bcrypt = require("bcrypt");
+
+exports.getUserById = (id) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM users WHERE id=?";
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve({ error: "User not found." });
+      } else {
+        const user = { id: row.id, email: row.email, username: row.username };
+        resolve(user);
+      }
+    });
+  });
+};
+
+exports.getUser = (email, password) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM users WHERE email=?";
+    db.get(sql, [email], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = { id: row.id, email: row.email, username: row.username };
+
+        bcrypt
+          .compare(password, row.hash)
+          .then((result) => {
+            if (result) {
+              resolve(user);
+            } else {
+              resolve(false);
+            }
+          })
+          .catch((err) => console.log(err));
+      }
+    });
+  });
+};
